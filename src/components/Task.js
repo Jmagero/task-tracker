4dd6b9e937e04328834f4242d5042368655a5da0
Jocyline Magero <jmagero54@gmail.com>
2022-04-12 16:14:48 -0500

    Add toggle function to onDoubleClick

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,7 @@
 import { FaTimes } from 'react-icons/fa'
-export const Task = ({task, onDelete}) => {
+export const Task = ({task, onDelete, toggle}) => {
    return(
-       <div>
+       <div onDoubleClick={() => toggle(task.id)}>
         <h3>{task.text}
             < FaTimes style={{color:'red'}} onClick={() => onDelete(task.id)}/>
         </h3>
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import { Task } from "./Task"
 
-export const Tasks = ({tasks, onDelete}) => {
+export const Tasks = ({tasks, onDelete, toggle}) => {
    return(
        <div>
            { tasks.length > 0 ?
                tasks.map((task) => (
-               <Task key={task.id} task={task} onDelete={onDelete} />
+               <Task key={task.id} task={task} onDelete={onDelete} toggle={toggle} />
                )):
                <h4>No Tasks</h4>}
        </div>
