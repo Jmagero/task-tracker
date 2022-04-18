b390174020f44c2119d6e18279c39a5875dafd9b
Jocyline Magero <jmagero54@gmail.com>
2022-04-18 14:15:09 -0500

    Fix eslint errrors

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
-import { FaTimes } from 'react-icons/fa'
-export const Task = ({task, onDelete, toggle}) => {
-   return(
-       <div onDoubleClick={() => toggle(task.id)}>
-        <h3>{task.text}
-            < FaTimes style={{color:'red'}} onClick={() => onDelete(task.id)}/>
-        </h3>
-        <p>{task.day}</p>
-       </div>
-   )
-}
+import { FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+const Task = ({ task, onDelete, toggle }) => (
+  <div onDoubleClick={() => toggle(task.id)}>
+    <h3>
+      {task.text}
+      <FaTimes style={{ color: 'red' }} onClick={() => onDelete(task.id)} />
+    </h3>
+    <p>{task.day}</p>
+  </div>
+);
+
+Task.propTypes = {
+  task: PropTypes.instanceOf(Object).isRequired,
+  onDelete: PropTypes.func.isRequired,
+  toggle: PropTypes.func.isRequired,
+};
+export default Task;
