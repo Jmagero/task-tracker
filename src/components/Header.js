7f9f6e25338c8d56577ede405c48d201ece65aee
Jocyline Magero <jmagero54@gmail.com>
2022-04-19 15:18:30 -0500

    Add button to only exact path

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 
-const Header = ({ showAddTask, onAdd }) => (
-  <header>
-    <h1>This is Header</h1>
-    <Button
-      text={showAddTask ? 'close' : 'Add'}
-      color={showAddTask ? 'red' : 'green'}
-      onClick={onAdd}
-    />
-  </header>
-);
+const Header = ({ showAddTask, onAdd }) => {
+  const location = useLocation();
+  return (
+    <header>
+      <h1>This is Header</h1>
+      {location.pathname === '/'
+     && (
+     <Button
+       text={showAddTask ? 'close' : 'Add'}
+       color={showAddTask ? 'red' : 'green'}
+       onClick={onAdd}
+     />
+     )}
+
+    </header>
+  );
+};
 
 Header.propTypes = {
   showAddTask: PropTypes.bool.isRequired,
